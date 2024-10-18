b6a335e3993b8b143dec074c505edd76c5585f48
parkjiwon <ji11won@naver.com>
2024-10-18 16:53:25 +0900

    feat : Application 추가

---
+++ app/src/main/java/kr/ac/konkuk/wastezero/application/Application.kt
@@ -0,0 +1,13 @@
+package kr.ac.konkuk.wastezero.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
