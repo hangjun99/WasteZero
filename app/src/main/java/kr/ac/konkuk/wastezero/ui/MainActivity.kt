1113bc0f7aaebd2ce84915f4fbfc172aaf804ccb
parkjiwon <ji11won@naver.com>
2024-10-29 17:38:52 +0900

    move : MainActivity 패키지 이동
    . -> ./ui 패키지 이동

--- app/src/main/java/kr/ac/konkuk/wastezero/ui/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/ui/MainActivity.kt
@@ -1,22 +1,24 @@
-package kr.ac.konkuk.wastezero
+package kr.ac.konkuk.wastezero.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import dagger.hilt.android.AndroidEntryPoint
+import kr.ac.konkuk.wastezero.R
+import kr.ac.konkuk.wastezero.databinding.ActivityMainBinding
+import kr.ac.konkuk.wastezero.util.base.BaseActivity
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(layoutResId = R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        /*enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
     }
 }
