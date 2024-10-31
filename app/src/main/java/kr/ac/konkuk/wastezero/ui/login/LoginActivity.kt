db6e117ff0b96b7f6b89c3e5721a4522eabf5cd7
parkjiwon <ji11won@naver.com>
2024-11-01 03:52:23 +0900

    feat : LoginActivity 추가

---
+++ app/src/main/java/kr/ac/konkuk/wastezero/ui/login/LoginActivity.kt
@@ -0,0 +1,32 @@
+package kr.ac.konkuk.wastezero.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
+import kr.ac.konkuk.wastezero.R
+import kr.ac.konkuk.wastezero.databinding.ActivityLoginBinding
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
