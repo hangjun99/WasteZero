2166d24b135a1fe7e440806b961fd0ece05ac13c
parkjiwon <ji11won@naver.com>
2024-11-06 11:07:06 +0900

    feat : base에 생명주기 함수 추가

--- app/src/main/java/kr/ac/konkuk/wastezero/util/base/BaseActivity.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/util/base/BaseActivity.kt
@@ -5,6 +5,7 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import timber.log.Timber
 
 abstract class BaseActivity<T : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int
@@ -16,5 +17,32 @@ abstract class BaseActivity<T : ViewDataBinding>(
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutResId)
         binding.lifecycleOwner = this
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 }
