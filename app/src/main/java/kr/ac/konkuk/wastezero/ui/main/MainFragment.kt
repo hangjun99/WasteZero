06555335080577bf77d40710ffeafb3ebcc39c2d
parkjiwon <ji11won@naver.com>
2024-11-29 17:16:11 +0900

    feat(MainFragment) : Navigation Component 적용
    - FragmentContainerView에 bottom_navigation_graph 적용
    - BottomNavigationView와 bottom_navigaiton_graph 연결

--- app/src/main/java/kr/ac/konkuk/wastezero/ui/main/MainFragment.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/ui/main/MainFragment.kt
@@ -2,6 +2,8 @@ package kr.ac.konkuk.wastezero.ui.main
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import kr.ac.konkuk.wastezero.R
 import kr.ac.konkuk.wastezero.databinding.FragmentMainBinding
 import kr.ac.konkuk.wastezero.util.base.BaseFragment
@@ -12,6 +14,20 @@ class MainFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun initBinding() {
+        super.initBinding()
+
+        binding.apply {
+            val navController =
+                childFragmentManager.findFragmentById(R.id.main_fragment_container_fcv)!!
+                    .findNavController()
+            mainBnv.setupWithNavController(navController)
+
+        }
+
+//        findNavController().navigate(R.id.action_mainFragment_to_searchFragment)
     }
 
 }
