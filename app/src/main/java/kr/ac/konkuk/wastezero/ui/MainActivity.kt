89642099d57c9e6fe1200b2af6995732dba3033b
parkjiwon <ji11won@naver.com>
2024-11-29 17:12:46 +0900

    feat(MainActivity) : 로그인 확인, Navigation Component 적용
    - checkLoginState : Firebase 로그인 여부 확인
    - FragmentContainerView와 main_graph를 연결

--- app/src/main/java/kr/ac/konkuk/wastezero/ui/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/ui/MainActivity.kt
@@ -1,12 +1,14 @@
-// MainActivity.kt
 package kr.ac.konkuk.wastezero.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kr.ac.konkuk.wastezero.R
 import kr.ac.konkuk.wastezero.databinding.ActivityMainBinding
-import kr.ac.konkuk.wastezero.ui.profile.ProfileFragment
 import kr.ac.konkuk.wastezero.util.base.BaseActivity
+import timber.log.Timber
 
 //@AndroidEntryPoint
 class MainActivity(
@@ -16,7 +18,7 @@ class MainActivity(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 초기 화면 설정 (메인 화면)
+        /*// 초기 화면 설정 (메인 화면)
         openFragment(null)
 
         // Bottom Navigation 아이템 선택 이벤트
@@ -36,10 +38,46 @@ class MainActivity(
 
                 else -> false
             }
-        }
+        }*/
+
+        Timber.d("onCreate")
+        checkLoginState(Firebase.auth.currentUser != null)
     }
 
-    // Fragment 전환 함수
+    override fun onStart() {
+        super.onStart()
+
+        Timber.d("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Timber.d("onResume")
+    }
+
+    override fun initBinding() {
+        super.initBinding()
+
+        val controller = getNavigationController()
+
+        binding.apply {
+
+        }
+
+    }
+
+    private fun checkLoginState(isLogin: Boolean) {
+        if (!isLogin) {
+            getNavigationController().navigate(R.id.action_mainFragment_to_login_graph)
+        }
+
+    }
+
+    private fun getNavigationController(): NavController =
+        supportFragmentManager.findFragmentById(R.id.main_navigation_host_fcv)!!.findNavController()
+
+    /*// Fragment 전환 함수
     private fun openFragment(fragment: Fragment?) {
         val transaction = supportFragmentManager.beginTransaction()
 
@@ -52,5 +90,5 @@ class MainActivity(
         }
 
         transaction.commit()
-    }
+    }*/
 }
