af2069bdc9338855b2d333346bbcaab1787af961
parkjiwon <ji11won@naver.com>
2024-12-09 19:11:04 +0900

    feat(Ingredient) : 파라미터 변경, 만료 확인 기능 추가
    - 파라미터 변경 : Date deprecated -> LocalDate로 변경
    - isExpired() : 유통기한 만료 여부 확인
    - calculateRestDate : 유통기한까지 남은 날 확인

--- app/src/main/java/kr/ac/konkuk/wastezero/domain/entity/Ingredient.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/domain/entity/Ingredient.kt
@@ -1,18 +1,26 @@
 package kr.ac.konkuk.wastezero.domain.entity
 
-import java.util.Date
+import java.time.Duration
+import java.time.LocalDate
+import java.time.temporal.ChronoField
+import java.time.temporal.ChronoUnit
 
 data class Ingredient(
     val id: Int,
     val name: String,
     val imageUrl: String,
-    val buyingDate: Date,
-    val expiryDate: Date,
+    val buyingDate: LocalDate,
+    val expiryDate: LocalDate,
     val isUsed: Boolean,
 ) {
     fun calculateRestDate(): Int {
-        val currentTime = System.currentTimeMillis()
-        val restTime = expiryDate.time - currentTime
-        return (restTime / (24 * 60 * 60 * 1000)).toInt()
+        val currentDay = LocalDate.now()
+        return if (!isExpired()) ChronoUnit.DAYS.between(currentDay, expiryDate).toInt() else -1
+    }
+
+    fun isExpired(): Boolean {
+        // 현재 날짜가 유통기한 날짜보다 뒤라면 T / 아직 전이거나 당일이면 F
+        val currentDate = LocalDate.now()
+        return currentDate.isAfter(expiryDate)
     }
 }
