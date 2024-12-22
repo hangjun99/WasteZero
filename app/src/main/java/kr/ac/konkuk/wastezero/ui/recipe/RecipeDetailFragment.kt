facea992ff58eb422fceb24a7d88e06eb0effa94
parkjiwon <ji11won@naver.com>
2024-12-23 05:17:26 +0900

    feat(RecipeDetail) : RecipeDetail 생성

--- app/src/main/java/kr/ac/konkuk/wastezero/ui/recipe/RecipeDetailFragment.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/ui/recipe/RecipeDetailFragment.kt
@@ -2,6 +2,8 @@ package kr.ac.konkuk.wastezero.ui.recipe
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import kr.ac.konkuk.wastezero.R
 import kr.ac.konkuk.wastezero.databinding.FragmentRecipeDetailBinding
 import kr.ac.konkuk.wastezero.util.base.BaseFragment
@@ -10,8 +12,18 @@ class RecipeDetailFragment(
 
 ) : BaseFragment<FragmentRecipeDetailBinding>(R.layout.fragment_recipe_detail) {
 
+    private val recipeUrl: String by lazy {
+        val args: RecipeDetailFragmentArgs by navArgs()
+        args.recipeUrl
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            recipeDetailWv.webViewClient = WebViewClient()
+            recipeDetailWv.loadUrl(recipeUrl)
+        }
     }
 
 }
