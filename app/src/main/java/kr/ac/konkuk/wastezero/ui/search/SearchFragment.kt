0d2475e456c937fc14ae1225f1d81eb5e092d705
parkjiwon <ji11won@naver.com>
2024-12-23 05:16:39 +0900

    feat(Search) : 검색 화면 생성

--- app/src/main/java/kr/ac/konkuk/wastezero/ui/search/SearchFragment.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/ui/search/SearchFragment.kt
@@ -2,16 +2,55 @@ package kr.ac.konkuk.wastezero.ui.search
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.ac.konkuk.wastezero.R
 import kr.ac.konkuk.wastezero.databinding.FragmentSearchBinding
+import kr.ac.konkuk.wastezero.domain.entity.Recipe
+import kr.ac.konkuk.wastezero.domain.entity.RecipeDetail
 import kr.ac.konkuk.wastezero.util.base.BaseFragment
 
 class SearchFragment(
 
 ) : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
 
+    private val args: SearchFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val searchTerm = args.ingredientName
+        setRecyclerView(getRecipeList(searchTerm))
+    }
+
+    private fun getRecipeList(term: String): List<RecipeAdapter.ItemType> {
+        val title = RecipeAdapter.ItemType.TitleItem("\"$term\"를 포함한 레시피")
+        // TODO : DB에서 term을 포함한 레시피 리스트를 가져오는 코드 작성
+        val recipes = RecipeDetail.potatoList.map {
+            RecipeAdapter.ItemType.RecipeItem(
+                Recipe(it.id, it.name, it.imageUrl, it.recipeUrl)
+            )
+        }
+        return listOf(title) + recipes
+    }
+
+    private fun setRecyclerView(result: List<RecipeAdapter.ItemType>) {
+        val adapter = RecipeAdapter(requireContext(), result).also {
+            it.setRecipeClickListener(object : RecipeAdapter.OnRecipeClickListener {
+                override fun onRecipeItemClick(item: Recipe) {
+                    val action =
+                        SearchFragmentDirections.actionSearchFragmentToRecipeDetailFragment(
+                            // TODO : item의 recipeUrl을 넘겨주는 코드 작성
+                            recipeUrl = item.recipeUrl
+                        )
+                    findNavController().navigate(action)
+                }
+            })
+        }
+
+        binding.searchRecipeListRv.adapter = adapter
+        binding.searchRecipeListRv.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
