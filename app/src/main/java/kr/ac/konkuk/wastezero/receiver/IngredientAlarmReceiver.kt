c2762ceee912ce323485cae928a0596659a7fac9
parkjiwon <ji11won@naver.com>
2024-12-23 13:16:03 +0900

    feat(IngredientAlarmReceiver) : BroadcastReceiver 생성

--- app/src/main/java/kr/ac/konkuk/wastezero/receiver/IngredientAlarmReceiver.kt
+++ app/src/main/java/kr/ac/konkuk/wastezero/receiver/IngredientAlarmReceiver.kt
@@ -1,4 +1,20 @@
 package kr.ac.konkuk.wastezero.receiver
 
-class IngredientAlarmReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import kr.ac.konkuk.wastezero.ui.main.MainFragment
+import timber.log.Timber
+
+class IngredientAlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.let {
+            if (intent.getStringExtra(MainFragment.NOTIFICATION_REQUEST_KEY_MAIN) == MainFragment.NOTIFICATION_REQUEST_CODE_MAIN) {
+                Toast.makeText(context, "Alarm Start", Toast.LENGTH_SHORT).show()
+                val count = intent.getIntExtra("count", 0)
+                Timber.d("myLog", "$count")
+            }
+        }
+    }
 }
